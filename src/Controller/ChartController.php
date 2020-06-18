c856b7bc46d6a707f357811ec820cd2bb185c9a8
mixno09 <mixno09@mail.ru>
2020-06-18 23:11:08 +0300

    Создана логика рендеринга графика.

--- src/Controller/ChartController.php
+++ src/Controller/ChartController.php
@@ -2,16 +2,78 @@
 
 namespace App\Controller;
 
+use Doctrine\DBAL\Connection;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ChartController extends AbstractController
 {
     /**
-     * @Route("/file/chart", name="chart")
+     * @var \Doctrine\DBAL\Connection
      */
-    public function index()
+    private $connection;
+
+    /**
+     * ChartController constructor.
+     * @param \Doctrine\DBAL\Connection $connection
+     */
+    public function __construct(Connection $connection)
     {
-        return $this->render('chart/index.html.twig');
+        $this->connection = $connection;
+    }
+
+    /**
+     * @Route("/departure/{airplane}/{date}/{departure}", name="departure")
+     * @param int $airplane
+     * @param int $departure
+     * @param string $date
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function index(int $airplane, string $date, int $departure): Response
+    {
+        $queryBuilder = $this->connection->createQueryBuilder();
+        $query = $queryBuilder
+            ->select('*')
+            ->from('flight_information')
+            ->where('airplane = ' . $queryBuilder->createPositionalParameter($airplane))
+            ->andWhere('date = ' . $queryBuilder->createPositionalParameter($date))
+            ->andWhere('departure = ' . $queryBuilder->createPositionalParameter($departure))
+            ->orderBy('time');
+        $statement = $query->execute();
+        $params = $statement->fetchAll();
+
+        $labels = [];
+        $t4Right = [];
+        $t4Left = [];
+        $alfaRudLeft = [];
+        $alfaRudRight = [];
+        $rndLeft = [];
+        $rndRight = [];
+        $rvdLeft = [];
+        $rvdRight = [];
+        foreach ($params as $param) {
+            $labels[] = $param['time'];
+            $t4Right[] = $param['t4_right'];
+            $t4Left[] = $param['t4_left'];
+            $alfaRudLeft[] = $param['alfa_rud_left'];
+            $alfaRudRight[] = $param['alfa_rud_right'];
+            $rndLeft[] = $param['rnd_left'];
+            $rndRight[] = $param['rnd_right'];
+            $rvdLeft[] = $param['rvd_left'];
+            $rvdRight[] = $param['rvd_right'];
+        }
+
+        return $this->render('chart/index.html.twig', [
+            'labels' => $labels,
+            't4Right' => $t4Right,
+            't4Left' => $t4Left,
+            'alfaLeft' => $alfaRudLeft,
+            'alfaRight' => $alfaRudRight,
+            'rndLeft' => $rndLeft,
+            'rndRight' => $rndRight,
+            'rvdLeft' => $rvdLeft,
+            'rvdRight' => $rvdRight,
+        ]);
     }
 }
